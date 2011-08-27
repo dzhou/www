02f9629c4fbebcffab10dc792769d663fbbe8b8b
Kefei Dan Zhou <kefei.zhou@gmail.com>
2011-08-26 21:59:49 -0400

    update

--- zhouk_com/download_euler_badge.sh
+++ zhouk_com/download_euler_badge.sh
@@ -1,1 +1,1 @@
-wget http://projecteuler.net/profile/dzhou.png -O /home/dzhou/github/www/zhouk_com/images/project_euler_dzhou.png
+wget http://projecteuler.net/profile/dzhou.png -O /home/dzhou/www/zhouk_com/images/project_euler_dzhou.png
